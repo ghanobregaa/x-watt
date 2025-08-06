c3ca2389e93cec2f6ac69528d447a0074b9d924f
Guilherme Nobrega <ghanobrega@gmail.com>
2025-08-06 17:50:41 +0100

    update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import styles from "./Navbar.module.css";
-
+import { motion } from "motion/react";
 function Navbar() {
   return (
-    <div className={styles.container}>
+    <motion.div
+      className={styles.container}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.5, duration: 1 }}
+    >
       <div className={styles.nav}>
-        {/* <img src="logo_white.svg" alt="logo" /> */}
+        <div className={styles.logoTitle}>
+          <img src="logo_text.svg" />
+        </div>
+        <div className={styles.row}>
+          <div className={styles.btn}>Produtos</div>
+          <div className={styles.btn}>Sobre nós</div>
+          <div className={styles.btn}>Contactos</div>
+        </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import styles from "./Home.module.css";
 import { motion } from "motion/react";
+import jsonData from "../../app.json";
 
 function Home() {
+  const [app, setApp] = useState({});
+  useEffect(() => {
+    setApp(jsonData);
+  });
   return (
     <>
       <Navbar />
-      <div className={styles.section1}>
-        <div className={styles.boxImg}>
-          <div className={styles.infoBox}>
-            <motion.h1
-              style={{ overflow: "hidden", whiteSpace: "nowrap", opacity: 0 }}
-              initial={{ width: 0 }}
-              animate={{ width: "100%", opacity: 1 }}
-              transition={{ duration: 1, delay: 0.5, ease: "easeInOut" }}
-            >
-              X-WATT
-            </motion.h1>
+      <section>
+        <div className={styles.center}>
+          <motion.div className={styles.box}>
             <motion.div
-              initial={{ y: 5 }}
+              initial={{ opacity: 0, y: 10 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}
-              className={styles.about}
+              transition={{ delay: 0.5, type: "spring", ease: "easeInOut" }}
+              className={styles.title}
+            >
+              Power Your Home with Clean Energy span>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.75, ease: "easeInOut" }}
+              className={styles.aboutBtn}
             >
               Saber mais
             </motion.div>
-          </div>
+          </motion.div>
         </div>
-      </div>
+        <motion.div className={styles.mainImg}></motion.div>
+      </section>
+      <section>
+        <motion.div
+          initial={{ opacity: 0, x: -10 }}
+          whileInView={{ opacity: 1, x: 0 }}
+          transition={{ delay: 0.25 }}
+        >
+          <img className={styles.product} src="powerwall.png" />
+        </motion.div>
+      </section>
     </>
   );
 }
